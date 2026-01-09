cd30c82ee0a86ab099e697079c764935650acd42
RydertHuGlIfE <varunchauhan2001dma@gmail.com>
2026-01-09 21:43:44 +0530

    fixed dragon and animations

--- src/components/NextSection.jsx
+++ src/components/NextSection.jsx
@@ -8,18 +8,28 @@ function NextSection() {
       <div ref={scroll} id="scroll">
         <img
           src="scroll_handle.svg "
-          className=" absolute -top-15 right-1/4 z-20"
+          className=" absolute -top-15 right-1/3 z-20"
           id="scroll"
           alt=""
         />
         <img
           src="scroll_paper.svg "
-          className="absolute -top-10 right-1/4 z-10"
+          className="absolute -top-10 right-1/3 z-10"
           id="scroll"
           alt=""
         />
       </div>
       <TransitionClouds scroll={scroll} />
+
+      {/* Prize Image */}
+      <div className="mt-800">
+        <img
+          src="PRIZE.png"
+          alt="Prize"
+          className="max-w-full h-auto"
+          style={{ transform: 'scale(1.25)' }}
+        />
+      </div>
     </section>
   );
 }
