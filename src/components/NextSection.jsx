23f9df9f4b2708d79b71d66557e9bd1cace1c040
RydertHuGlIfE <varunchauhan2001dma@gmail.com>
2026-01-09 21:20:37 +0530

    added prize image

--- src/components/NextSection.jsx
+++ src/components/NextSection.jsx
@@ -6,9 +6,14 @@ function NextSection() {
   return (
     <section
       ref={container}
-      className=" relative w-full h-screen bg-no-repeat bg-center bg-contain bg-fixed flex justify-center items-center"
+      className="relative w-full h-screen bg-no-repeat bg-center bg-contain bg-fixed flex flex-col justify-center items-center"
     >
       <TransitionClouds scope={container} />
+
+      {/* Prize Image with spacing    increase mt to make the image go low*/}
+      <div className="mt-580 mb-8">
+        <img src="/PRIZE.png" alt="Prize" className="max-w-full h-auto" style={{ transform: 'scale(1.25)' }} />
+      </div>
     </section>
   );
 }
