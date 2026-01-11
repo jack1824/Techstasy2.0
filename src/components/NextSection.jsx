f6d3456cd36e91bc8e42740cfdf1315a39dc9447
PerseusKyogre09 <kyogre.perseus09@gmail.com>
2026-01-11 17:39:10 +0530

    scroll timer countdown resize fix

--- src/components/NextSection.jsx
+++ src/components/NextSection.jsx
@@ -17,13 +17,13 @@ function NextSection() {
         />
         <div className="w-[50%] absolute top-0 sm:-top-10 right-1/4 z-10 flex justify-center items-center">
           <img src="scroll_paper.svg " className="" id="scroll" alt="" />
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4 w-full">
-            <h2 className="text-[4vw]  sm:text-4xl md:text-5xl font-black text-center leading-none tracking-wider text-outline-lg font-kungfu uppercase">
+          <div className="absolute top-1/2 left-[48%] transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4 w-full">
+            <h2 className="text-[3.5vw] sm:text-2xl md:text-3xl font-black text-center leading-none tracking-wider text-outline-lg font-kungfu uppercase">
               HACKATHON <br /> STARTS IN
             </h2>
 
-            <div className="bg-white border-4 border-black rounded-full px-6 py-1 md:px-5 md:py-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
-              <span className="text-[4vw] md:text-3xl font-black tracking-widest font-kungfu">
+            <div className="bg-white border-4 border-black rounded-full px-2 py-0.5 sm:px-3 sm:py-1 md:px-4 md:py-1.5 lg:px-5 lg:py-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+              <span className="text-[2vw] sm:text-sm md:text-lg lg:text-xl xl:text-2xl font-black tracking-widest font-kungfu">
                 {days}D {hours}H {minutes}M {seconds}S
               </span>
             </div>
